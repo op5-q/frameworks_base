b80b0fa363b577564aae94fd49d3d59d1fa77bcc
Nico60 <ndhouailly@gmail.com>
2019-03-14 07:04:00 -0400

    TrafficSB: Hide arrows in statusbar traffic indicators
    arrow-edits:
    adapt code to our dynamic arrows method
    
    Change-Id: Ie50dbe3f54495d4b5c0c0a51969df595de7a1386
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/StatusBarNetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/StatusBarNetworkTraffic.java
@@ -84,6 +84,7 @@ public class StatusBarNetworkTraffic extends NetworkTraffic implements StatusIco
     private boolean mSystemIconVisible = true;
     private boolean indicatorUp = false;
     private boolean indicatorDown = false;
+    private boolean mHideArrow;
 
     private boolean mScreenOn = true;
 
@@ -141,7 +142,8 @@ public class StatusBarNetworkTraffic extends NetworkTraffic implements StatusIco
                 mTrafficVisible = true;
             }
             updateVisibility();
-            updateTrafficDrawable();
+            if (!mHideArrow)
+                updateTrafficDrawable();
 
             // Post delayed message to refresh in ~1000ms
             totalRxBytes = newTotalRxBytes;
@@ -197,6 +199,9 @@ public class StatusBarNetworkTraffic extends NetworkTraffic implements StatusIco
                     this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD), false,
+                    this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.NETWORK_TRAFFIC_HIDEARROW), false,
                     this, UserHandle.USER_ALL);
         }
 
@@ -313,6 +318,9 @@ public class StatusBarNetworkTraffic extends NetworkTraffic implements StatusIco
         mAutoHideThreshold = Settings.System.getIntForUser(resolver,
                 Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 0,
                 UserHandle.USER_CURRENT);
+        mHideArrow = Settings.System.getIntForUser(resolver,
+                Settings.System.NETWORK_TRAFFIC_HIDEARROW, 0,
+                UserHandle.USER_CURRENT) == 1;
     }
 
     private void clearHandlerCallbacks() {
@@ -323,7 +331,7 @@ public class StatusBarNetworkTraffic extends NetworkTraffic implements StatusIco
 
     private void updateTrafficDrawable() {
         int indicatorDrawable;
-        if (mIsEnabled) {
+        if (mIsEnabled && !mHideArrow) {
             if (indicatorUp) {
                 indicatorDrawable = R.drawable.stat_sys_network_traffic_up_arrow;
                 Drawable d = getContext().getDrawable(indicatorDrawable);
@@ -339,7 +347,9 @@ public class StatusBarNetworkTraffic extends NetworkTraffic implements StatusIco
             } else {
                 setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
             }
-	}
+        } else {
+            setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+        }
         setTextColor(mTintColor);
         indicatorUp = false;
         indicatorDown = false;

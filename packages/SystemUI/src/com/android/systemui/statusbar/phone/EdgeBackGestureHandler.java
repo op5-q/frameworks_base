4376e7efeb74e53153453b017e3864186a87a8f5
Adhitya Mohan <me@adhityamohan.in>
2019-10-24 16:25:22 +0530

    Revert "SystemUI: Fix navbar toggle"
    This reverts commit 15aed7ad62096ba14369a7c855018c1269e0877f.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -217,7 +217,7 @@ public class EdgeBackGestureHandler implements DisplayListener {
     }
 
     private void updateIsEnabled() {
-        boolean isEnabled = mIsGesturalModeEnabled;
+        boolean isEnabled = mIsAttached && mIsGesturalModeEnabled;
         if (isEnabled == mIsEnabled) {
             return;
         }
